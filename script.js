92711b4476b357807cc607dab337cd4620cdcc73
Mitushi Vishwakarma <vishmitushi.github.com>
2020-12-14 02:25:12 +0530

    project-2
    Admin-panel

---
+++ script.js
@@ -0,0 +1,69 @@
+function openNav() {
+    document.getElementById("Nav-left").style.width = "240px";
+    document.getElementById("main").style.marginLeft = "240px";
+  }
+  
+  function closeNav() {
+    document.getElementById("Nav-left").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
+
+  function openNavright() {
+    document.getElementById("Nav-right").style.width = "400px";
+    document.getElementById("list1").style.backgroundColor = "#ff8306"
+    document.getElementById("list-icon1").style.color = "white"
+  }
+  
+  function closeNavright() {
+    document.getElementById("Nav-right").style.width = "0";
+    document.getElementById("list1").style.backgroundColor = "white"
+    document.getElementById("list-icon1").style.color = "#949494"
+  }
+  function openNavright2() {
+    document.getElementById("Nav-right2").style.width = "400px";
+    document.getElementById("list2").style.backgroundColor = "#ff8306"
+    document.getElementById("list-icon2").style.color = "white"
+  }
+  
+  function closeNavright2() {
+    document.getElementById("Nav-right2").style.width = "0";
+    document.getElementById("list2").style.backgroundColor = "white"
+    document.getElementById("list-icon2").style.color = "#949494"
+  }
+
+  function openNavright3() {
+    document.getElementById("Nav-right3").style.width = "400px";
+    document.getElementById("list3").style.backgroundColor = "#ff8306"
+    document.getElementById("list-icon3").style.color = "white"
+  }
+  
+  function closeNavright3() {
+    document.getElementById("Nav-right3").style.width = "0";
+    document.getElementById("list3").style.backgroundColor = "white"
+    document.getElementById("list-icon3").style.color = "#949494"
+  }
+
+  function openNavright4() {
+    document.getElementById("Nav-right4").style.width = "400px";
+    document.getElementById("list4").style.backgroundColor = "#ff8306"
+    document.getElementById("list-icon4").style.color = "white"
+  }
+  
+  function closeNavright4() {
+    document.getElementById("Nav-right4").style.width = "0";
+    document.getElementById("list4").style.backgroundColor = "white"
+    document.getElementById("list-icon4").style.color = "#949494"
+  }
+
+  function openNavright5() {
+    document.getElementById("Nav-right5").style.width = "400px";
+    document.getElementById("list5").style.backgroundColor = "#ff8306"
+    document.getElementById("list-icon5").style.color = "white"
+  }
+  
+  function closeNavright5() {
+    document.getElementById("Nav-right5").style.width = "0";
+    document.getElementById("list5").style.backgroundColor = "white"
+    document.getElementById("list-icon5").style.color = "#949494"
+  }
+
